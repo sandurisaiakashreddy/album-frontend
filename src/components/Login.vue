58aab87edbb996853f48cfd5647840621ad5a64d
sai.sanduri <sai.sanduri@eagles.oc.edu>
2022-05-26 10:58:14 -0500

    Merged UI and API for login and register.

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,33 +2,66 @@
 <div>
    <br>
 <div class="w3-card-4">
-  <div  class="w3-container w3-blue">
+  <center>
+    <div  class="w3-container w3-blue" style="width: 65%;">
 
  <h4> Login</h4>
        </div>
 
        <h4><div class="error w3-text-red" v-html="error"></div></h4>
        <br>
-       <h4>Please Enter Email</h4>
-       <input type="text"  name="email" class="w3-input w3-border w3-sand"  placeholder="email"
+       <h4>Please Provide your Email</h4>
+        <form name="album-login-user"
+       autocomplete="off">
+       <input type="text"  name="email" class="w3-input" style="width: 50%;"  placeholder="Enter email"
     v-model="email"
    />
    <br>
-   <h4>Please enter Password</h4>
-   <input type="password" class="w3-input w3-border w3-sand"
+   <h4>Please Provide your Password</h4>
+   <input  class="w3-input" style="width: 50%;"
           name="password"
-          placeholder="Enter the Password"
-          v-model="password"
-   /><br>
+          placeholder="Enter password"
+          v-model="password" autocomplete="new-password"
+   />
+   </form>
    <br>
-
-    <button @click="login" class="w3-btn w3-blue">log-in</button>
+   <br>
+</center>
+    <button @click="login" class="w3-btn w3-blue">login</button>
     <br><br>
 </div>
 </div>
 </template>
 
 <script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: null
+    }
+  },
+  methods: {
+    async login () {
+      try {
+        console.log('Login button', this.email, this.password)
+        const response = await AuthenticationService.login({
+          email: this.email,
+          password: this.password
+        })
+        console.log(response.data.token)
+        this.error = 'Login sucessfull'
+      //   this.email = ' '
+      //   this.password = ' '
+        console.log(response)
+      } catch (error) {
+        this.error = error.response.data.error
+      }
+    }
+  }
+}
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -5,13 +5,18 @@ import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import { sync } from 'vuex-router-sync'
+import routestore from '@/state/routestore'
 Vue.config.productionTip = false
 
 Vue.use(Vuetify)
+
+sync(routestore, router)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  routestore,
   components: { App },
   template: '<App/>'
 })
